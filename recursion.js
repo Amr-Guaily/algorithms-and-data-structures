a01fcae0383f20b6ad9e8deec26b765bfb6b1c89
Amr Guaily <amrguaily@gmail.com>
2022-11-01 00:54:10 +0200

    Recursion Examples

---
+++ recursion.js
@@ -0,0 +1,102 @@
+// ? Calculate Factorial:
+function fact(num) {
+  // Base Case
+  if (num == 1) return 1;
+
+  return num * fact(num - 1);
+}
+fact(4); // 24
+
+// -------------------------------------------------------------- ########## ---------------------------------------------------------------------
+
+// ? Reverse String:
+function reverseString(str) {
+  if (str.length === 1) return str;
+
+  return reverseString(str.substring(1)) + str.charAt(0);
+}
+reverseString('hello'); // "olleh"
+
+// -------------------------------------------------------------- ########## ---------------------------------------------------------------------
+
+// ? Palidroms => Are these unique words, where we can spell the same word forward and backword.
+function isPalidrom(str) {
+  // Base Case
+  if (str.length == 1 || str.length == 0) return true;
+
+  if (str[0] == str[str.length - 1]) {
+    return isPalidrom(str.substring(1, str.length - 1));
+  }
+
+  return false;
+}
+isPalidrom('kayak'); // true
+
+// -------------------------------------------------------------- ########## ---------------------------------------------------------------------
+
+// ? Decimal to Binary
+function decimalToBinary(num) {
+  // Base Case
+  if (num == 0) return 0;
+
+  return (num % 2) + 10 * decimalToBinary(Math.floor(num / 2));
+}
+decimalToBinary(233); // 11101001
+
+// -------------------------------------------------------------- ########## ---------------------------------------------------------------------
+
+// ? Sum of natural number
+function sumUp(num) {
+  if (num <= 1) return 1;
+
+  return num + sumUp(num - 1);
+}
+sumUp(10); // 55
+
+// -------------------------------------------------------------- ########## ---------------------------------------------------------------------
+
+// ? Binary Search using recursion
+function recursiveBinarySearch(list, target, start = 0, end = list.length - 1) {
+  // Base-Case => If the item isn't exist
+  if (start > end) return -1;
+
+  let mid = Math.floor((start + end) / 2),
+    guess = list[mid];
+
+  if (guess === target) return mid;
+
+  if (guess > target)
+    return recursiveBinarySearch(list, target, start, mid - 1);
+
+  if (guess < target) return recursiveBinarySearch(list, target, mid + 1);
+}
+recursiveBinarySearch([1, 3, 5, 7, 9], 3); // 1
+
+// -------------------------------------------------------------- ########## ---------------------------------------------------------------------
+
+// ? Fibonacci Series
+function fib(num) {
+  // Base Case
+  if (num == 1 || num == 2) return 1;
+
+  return fib(num - 1) + fib(num - 2);
+}
+
+fib(8); // 21
+
+// Todo: Optimize Fibonacci Series Using Memoization
+function optimizedFib(num) {
+  // Base Case
+  if (num == 1 || num == 2) return 1;
+
+  // cach = {3: 2, 4: 3, 5: 5, ..etc}
+  const cach = {};
+  if (cach[num]) return num;
+
+  cach[num] = fib(num - 1) + fib(num - 2);
+
+  console.log(cach);
+  return cach[num];
+}
+
+optimizedFib(8); // 21
